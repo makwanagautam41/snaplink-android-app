c70ed0cb18ecb45434216c0145d34002bfd93177
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-13 22:03:31 +0530

    add proper navigating from the splash screen for logged in and logged out user behaviour

--- app/src/main/java/com/example/snaplink/Splash_screen.kt
+++ app/src/main/java/com/example/snaplink/Splash_screen.kt
@@ -8,18 +8,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.snaplink.network.TokenManager
 
 class Splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        Handler(Looper.getMainLooper()).
-        postDelayed({
-            //move to MainActivity
-            val intent = Intent(this, HomeActivityKt::class.java)
+        
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Check if user is logged in
+            val intent = if (TokenManager.isLoggedIn()) {
+                // User is logged in, navigate to Home
+                Intent(this, HomeActivityKt::class.java)
+            } else {
+                // User is not logged in, navigate to Login
+                Intent(this, LoginActivity::class.java)
+            }
             startActivity(intent)
             finish()
-        },3000)
+        }, 3000)
     }
 }
