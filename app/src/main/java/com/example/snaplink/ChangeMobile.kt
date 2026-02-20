80ea097c2dd72a8b1b629611e14bda556bca899b
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-20 21:36:49 +0530

    add change email and mobile pages ui

--- app/src/main/java/com/example/snaplink/ChangeMobile.kt
+++ app/src/main/java/com/example/snaplink/ChangeMobile.kt
@@ -1,20 +1,37 @@
 package com.example.snaplink
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ChangeMobile : AppCompatActivity() {
+    private lateinit var btnBack: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_change_mobile)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initViews()
+        setupListeners()
+    }
+
+    private fun initViews() {
+        btnBack = findViewById(R.id.btnBack)
+    }
+
+    private fun setupListeners() {
+        btnBack.setOnClickListener {
+            finish()
         }
     }
 }
