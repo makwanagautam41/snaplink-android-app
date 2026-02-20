8f035faf5efbc0238efea9ac5e410f79894d2819
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-20 23:25:35 +0530

    add account deletion and deactivation pages

--- app/src/main/java/com/example/snaplink/ui/fragments/AccountOwnershipFragment.kt
+++ app/src/main/java/com/example/snaplink/ui/fragments/AccountOwnershipFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import com.example.snaplink.R
+import com.example.snaplink.ui.activities.MainActivity
 
 class AccountOwnershipFragment : Fragment() {
     private lateinit var btnBack: ImageView
+    private lateinit var accountDeactivationField: RelativeLayout
+    private lateinit var accountDeletionField: RelativeLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +27,17 @@ class AccountOwnershipFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         btnBack = view.findViewById(R.id.btnBack)
+        accountDeactivationField = view.findViewById(R.id.accountDeactivationField)
+        accountDeletionField = view.findViewById(R.id.accountDeletionField)
+
         btnBack.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+        accountDeactivationField.setOnClickListener {
+            (activity as? MainActivity)?.navigateToFragment(AccountDeactivationFragment())
+        }
+        accountDeletionField.setOnClickListener {
+            (activity as? MainActivity)?.navigateToFragment(AccountDeletionFragment())
         }
     }
 }
