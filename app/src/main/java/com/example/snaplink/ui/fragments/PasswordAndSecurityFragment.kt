42622cfb20357762f6533e219c3d53108438a29c
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-21 21:29:40 +0530

    fix the ui design alighnment in few pages

--- app/src/main/java/com/example/snaplink/ui/fragments/PasswordAndSecurityFragment.kt
+++ app/src/main/java/com/example/snaplink/ui/fragments/PasswordAndSecurityFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.snaplink.R
+import com.example.snaplink.ui.activities.MainActivity
 
 class PasswordAndSecurityFragment : Fragment() {
     private lateinit var btnBack: ImageView
+    private lateinit var changePasswordField: RelativeLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,8 +28,13 @@ class PasswordAndSecurityFragment : Fragment() {
 
         try {
             btnBack = view.findViewById(R.id.btnBack)
+            changePasswordField = view.findViewById(R.id.changePasswordField)
             btnBack.setOnClickListener {
                 parentFragmentManager.popBackStack()
+            }
+
+            changePasswordField.setOnClickListener {
+                (activity as? MainActivity)?.navigateToFragment(ChangePasswordFragment())
             }
         } catch (e: Exception) {
             e.printStackTrace()
