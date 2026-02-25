a8a6238850240753a85ef9ea2592ad2add8785de
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-25 09:32:41 +0530

    add story page design only

---
+++ app/src/main/java/com/example/snaplink/ui/fragments/MyStoriesFragment.kt
@@ -0,0 +1,12 @@
+package com.example.snaplink.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.snaplink.R
+
+class MyStoriesFragment : Fragment() {
+
+}
